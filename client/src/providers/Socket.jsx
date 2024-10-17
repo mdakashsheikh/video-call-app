a1e8c7fc814ab60cd61f379f1bdec2936b713e3c
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-10-18 03:26:12 +0600

    socket add in client

---
+++ client/src/providers/Socket.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useMemo } from "react";
+import { io } from 'socket.io-client'
+
+const SocketContext = React.createContext(null);
+
+export const SocketProvider = (props) => {
+    const socket = useMemo(() => io({
+        host: 'localhost',
+        port: 8001,
+    }), [])
+
+    return (
+        <SocketContext.Provider value={{ socket }}>
+            {props.children}
+        </SocketContext.Provider>
+    )
+}
+
